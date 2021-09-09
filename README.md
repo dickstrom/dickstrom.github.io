37e19545a8be44fce7f56e0072e05e2e4d0e600d
Dick Ström <83727995+dickstrom@users.noreply.github.com>
2021-09-09 13:36:12 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# dickstrom.github.io
-To test my coding with
+👋 Hi, I’m @dickstrom and I will learn to code in mid life.
+👀 I like tech in all forms.
+🌱 I’m currently learning Javascript,HTML and CSS.
+💞️ I’m looking forward to learn other different program languages.
---
+++ myFunction.js
@@ -0,0 +1,6 @@
+function greeting() {
+    document.getElementById("hello").innerHTML="Hello!<br> Nice to see yaa. You are really velcome. <br>I think you are one of the best programmers!";
+  }
+  function coding() {
+    document.getElementById("code").innerHTML="This is hard but it works..are you a proffessional coder, I need help, call me on 5555555555 and so on..Is it possible for me to work with Javascript?";
+  }
