145b4732d9601c1a9210ec22254bdb66a509e06f
Dick Ström <dick5strom@gmail.com>
2021-09-09 15:53:06 +0200

    new button

--- myFunction.js
+++ myFunction.js
@@ -4,3 +4,11 @@ function greeting() {
   function coding() {
     document.getElementById("code").innerHTML="This is hard but it works..are you a proffessional coder, I need help, call me on 5555555555 and so on..Is it possible for me to work with Javascript?";
   }
+  function hi() {
+    var name = prompt("What is your name?");
+    var result = ("I did Javascript Mr "  + " " +  name);
+ document.getElementById("hi").innerHTML=result;
+ console.log(result);
+  }
+ 
+  
