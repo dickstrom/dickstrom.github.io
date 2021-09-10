085a0ae87d3a0b33b3fb0960c828cadf3a37be9f
Dick Ström <dick5strom@gmail.com>
2021-09-10 15:12:04 +0200

    button info

--- myFunction.js
+++ myFunction.js
@@ -2,7 +2,7 @@ function greeting() {
   document.getElementById("hello" ).innerHTML=prompt("Hello! Write something and it will show under this button");
 }
 function coding(){
-  document.getElementById("code").innerHTML=("You are the proffessional coder I would like to get in touch with.");
+  document.getElementById("code").innerHTML=("You are a proffessional coder, I would like to get in touch with you.I will send you a letter.");
 }
 function hi() {
   var name = prompt("What is your name?");
