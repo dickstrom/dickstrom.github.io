af450d4dbc709df98497d8bf5d6890849be41627
Dick Ström <dick5strom@gmail.com>
2021-09-19 13:10:20 +0200

    updated function coding

--- myFunction.js
+++ myFunction.js
@@ -2,7 +2,7 @@ function greeting() {
   document.getElementById("hello" ).innerHTML=prompt("Hello! Write something and it will show under this button");
 }
 function coding(){
-  document.getElementById("code").innerHTML=("You are a proffessional coder, I would like to get in touch with you.I will send you a letter.");
+  document.getElementById("code").innerHTML=("I am going to apply for Academic works as a 53 year old, do they care about my age or not? What will be the hardest thing to learn regarding coding? Can I get a job as a programmer next year?");
 }
 function hi() {
   var name = prompt("What is your name?");
