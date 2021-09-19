59d227f1466b634a922b88be54f3f6436396e55d
Dick Ström <dick5strom@gmail.com>
2021-09-19 13:15:51 +0200

    Update greeting
    trying to add maximumLength

--- myFunction.js
+++ myFunction.js
@@ -1,5 +1,5 @@
 function greeting() {
-  document.getElementById("hello" ).innerHTML=prompt("Hello! Write something and it will show under this button");
+  document.getElementById("hello" ).innerHTML=prompt("Hello! Write something and it will show under this button").maximumLength(30);
 }
 function coding(){
   document.getElementById("code").innerHTML=("I am going to apply for Academic works as a 53 year old, do they care about my age or not? What will be the hardest thing to learn regarding coding? Can I get a job as a programmer next year?");
